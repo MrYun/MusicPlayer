b3a31f28934be8e676aea81b8a4a68d6ed91db81
MrYun <azhaoyun66@gmail.com>
2015-10-30 19:17:54 +0800

    创建项目

---
+++ ZYMusicPlayer/ZYMusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYMusicPlayer
+//
+//  Created by 赵云 on 15/10/30.
+//  Copyright © 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYMusicPlayer/ZYMusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYMusicPlayer
+//
+//  Created by 赵云 on 15/10/30.
+//  Copyright © 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
